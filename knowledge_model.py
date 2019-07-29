62a580cf6b4ed246dcb358e04e5b01a04383f925
myname21-meet <myname21@meet.mit.edu>
2019-07-29 03:31:07 -0400

    SQLAlchemy lab 1 part 1

--- knowledge_model.py
+++ knowledge_model.py
@@ -6,6 +6,23 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 class Knowledge(Base):
+	__tablename__ = "Topics to learn about"
+	topic_id= Column(String, primary_key = True )
+	art_title = Column(String)
+	wiki_link = Column(String)
+	rating = Column(Integer)
+	def __repr__(self):
+		return ("if you want to learn about {}\n, "
+
+				"you should check this link on wikipedia {}\n, "
+				"we gave it a rating of {}").format(
+					self.art_title,
+					self.wiki_link,
+					self.rating)
+
+article_1 = Knowledge(art_title = "Quantum Physics", wiki_link = 'https://en.wikipedia.org/wiki/Quantum_mechanics', rating = 7)
+print(article_1)
+
 	# Create a table with 4 columns
 	# The first column will be the primary key
 	# The second column should be a string representing
@@ -14,4 +31,3 @@ class Knowledge(Base):
 	# topic of the article. The last column will be
 	# an integer, representing your rating of the article.
 
-	pass
